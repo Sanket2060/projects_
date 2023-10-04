654787c7e19aabc7aa3996d62285e21dcb99c6ef
Sanket2060 <114918951+Sanket2060@users.noreply.github.com>
2023-10-04 21:39:14 +0545

    guessthenumberdone

--- 4-GuessTheNumber/chaiaurcode.js
+++ 4-GuessTheNumber/chaiaurcode.js
@@ -1,19 +1,45 @@
 let guessField = document.querySelector('#guessField');
 let subt = document.querySelector('#subt');
 let correct = document.querySelector('.correct');
-
+let form = document.querySelector('.form');
+let guessRemainingelem = document.querySelector('.lastResult');
+let prevguess = document.querySelector('.guesses');
+let lowOrHi = document.querySelector('.lowOrHi');
 const resultValue = Math.ceil(Math.random() * 100);
 console.log(resultValue);
-console.log("Hello");
+let guessValue;
 let remainingGuess = 10;
-while (remainingGuess != 0) {
-subt.addEventListener('click', () => {
-    let guessValue = guessField.value;
-    let previousGuess = guessValue;
-    if (resultValue == guessValue) {
-      correct.innerHTML="Congrats";
-      break;
+form.addEventListener('submit', operation);
+
+function operation(e) {
+  if (remainingGuess > 0) {
+    e.preventDefault();
+    guessValue = guessField.value;
+    if (guessValue == ' ') {
+      console.log('Please enter a number'); //Premade ui vayera console ma print gareko
+      console.log(guessValue);
+    } else {
+      if (guessValue == resultValue) {
+        correct.innerHTML = 'Correct value.You won';
+      } else if (guessValue > resultValue) {
+        correct.innerHTML = 'Incorrect.Try again';
+        remainingGuess--;
+        guessRemainingelem.innerHTML = +`${remainingGuess}`;
+        lowOrHi.innerHTML = 'Try lower';
+      } else {
+        correct.innerHTML = 'Incorrect.Try again';
+        remainingGuess--;
+        guessRemainingelem.innerHTML = +`${remainingGuess}`;
+        lowOrHi.innerHTML = 'Try higher';
+      }
     }
-    remainingGuess--;
-  })
+  } else {
+    e.preventDefault();
+    correct.innerHTML = 'Game over.0 chances left.Reload to replay.';
+  }
+  let prevguessvalue = guessValue;
+  prevguess.innerHTML = `${prevguessvalue}`;
+  guessField.value = ' ';
 }
+
+//10 times wrong value ayo vane Game over.0 chances left vanxa tara tespaxi program
